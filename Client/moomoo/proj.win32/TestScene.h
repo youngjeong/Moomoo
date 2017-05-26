e4c62426c6a40a5fc44f0e6eb28fb3eab357988b
FinnYu <ryj1549@gmail.com>
2017-05-26 15:05:38 +0900

    basic client settings, add test scene

---
+++ Client/moomoo/proj.win32/TestScene.h
@@ -0,0 +1,20 @@
+#ifndef __TESTSCENE_SCENE_H__
+#define __TESTSCENE_SCENE_H__
+
+#include "cocos2d.h"
+
+class TestScene : public cocos2d::Scene
+{
+public:
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+
+	// a selector callback
+	void menuCloseCallback(cocos2d::Ref* pSender);
+
+	// implement the "static create()" method manually
+	CREATE_FUNC(TestScene);
+};
+
+#endif // __TESTSCENE_SCENE_H__
