d5c31c9136b9c0061736d348fb7863196a6d1289
FinnYu <ryj1549@gmail.com>
2017-05-28 17:50:11 +0900

    modify git ignore setting

---
+++ Client/moomoo/Assets/Follower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour {
+    public GameObject objectToFollow;
+
+    public float speed = 5.0f;
+
+    void Update()
+    {
+        float interpolation = speed * Time.deltaTime;
+
+        Vector3 position = this.transform.position;
+        position.z = Mathf.Lerp(this.transform.position.z, objectToFollow.transform.position.z, interpolation);
+        position.x = Mathf.Lerp(this.transform.position.x, objectToFollow.transform.position.x, interpolation);
+
+        this.transform.position = position;
+    }
+}
