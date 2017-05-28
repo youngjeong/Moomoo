411f3aadc341d7c614ad31f9c024a38be91dfbed
JeongEuiJoon <dordong327@gmail.com>
2017-05-28 20:27:53 +0900

    initial_controller_classes

---
+++ Server/InRoomController.cpp
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   InRoomController.cpp
+ * Author: admin
+ * 
+ * Created on 2017년 5월 28일 (일), 오후 8:05
+ */
+
+#include "InRoomController.h"
+
+void InRoomController::checkReadyState() {
+
+}
+
+void InRoomController::gameStart() {
+
+}
+
+
+InRoomController::InRoomController() {
+}
+
+InRoomController::InRoomController(const InRoomController& orig) {
+}
+
+InRoomController::~InRoomController() {
+}
+
---
+++ Server/Room.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Room.cpp
+ * Author: admin
+ * 
+ * Created on 2017년 5월 28일 (일), 오후 8:08
+ */
+
+#include "Room.h"
+
+Room::Room() {
+}
+
+Room::Room(const Room& orig) {
+}
+
+Room::~Room() {
+}
+
