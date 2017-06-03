5b393cc2e002ae0a4805237f08d6ef44185ba644
Sun-woo, Lee <eldkqmfhf@gmail.com>
2017-06-03 17:44:04 +1000

    fix typo at errorcode

--- Server/nbproject/Makefile-variables.mk
+++ Server/nbproject/Makefile-variables.mk
@@ -15,13 +15,13 @@ CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=server.tar
 CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/server.tar
 # Release configuration
-CND_PLATFORM_Release=GNU-Linux
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_PLATFORM_Release=None-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/None-Linux
 CND_ARTIFACT_NAME_Release=server
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/server
-CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_ARTIFACT_PATH_Release=dist/Release/None-Linux/server
+CND_PACKAGE_DIR_Release=dist/Release/None-Linux/package
 CND_PACKAGE_NAME_Release=server.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/server.tar
+CND_PACKAGE_PATH_Release=dist/Release/None-Linux/package/server.tar
 #
 # include compiler specific variables
 #
